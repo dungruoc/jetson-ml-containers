5fbde56cea1d8f6531551c5abc5a6734a7245f8c
dungruoc <minhdung.dang@gmail.com>
2020-10-22 22:49:30 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build -t l4t-skimage:latest -f Dockerfile.skimage .
 
 docker build -t l4t-skimage-cuda:latest -f Dockerfile.skimage-cuda .
 
-docker build -t l4t-skimage-cuda-torch -f Dockerfile.skimage-cuda-pytorch .
+docker build -t l4t-skimage-cuda-torch:latest -f Dockerfile.skimage-cuda-pytorch .
