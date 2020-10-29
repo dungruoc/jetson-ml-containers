b347217dca375a705376352da59a5fcb23ddbed1
dungruoc <minhdung.dang@gmail.com>
2020-10-29 16:13:33 +0700

    Separate 4.4 and 4.3 compatible

--- build.sh
+++ build.sh
@@ -1,17 +1,29 @@
 ARM_UBUNTU_IMG=arm64v8/ubuntu:18.04
-L4T_SKIMAGE_IMG=l4t-skimage:latest
-L4T_SKIMAGE_CUDA_IMG=l4t-skimage-cuda:latest
-L4T_SKIMAGE_CUDA_TORCH_IMG=l4t-skimage-cuda-torch:latest
-L4T_SKIMAGE_CUDA_GST_OPENCV_IMG=l4t-skimage-cuda-gst-opencv:latest
-JP_VERSION=44
-L4T_SKIMAGE_CUDA_TF_IMG=l4t-skimage-cuda-tf:latest
+JETSON_SKIMAGE_IMG=jetson:skimage
 
-docker build -t $L4T_SKIMAGE_IMG --build-arg "BASE_IMAGE=${ARM_UBUNTU_IMG}" -f Dockerfile.skimage .
+docker build -t $JETSON_SKIMAGE_IMG --build-arg "BASE_IMAGE=${ARM_UBUNTU_IMG}" -f Dockerfile.skimage .
 
-docker build -t $L4T_SKIMAGE_CUDA_IMG --build-arg "BASE_IMAGE=${L4T_SKIMAGE_IMG}" -f Dockerfile.cuda .
+# JETSON pack 4.4 compatible
 
-docker build -t ${L4T_SKIMAGE_CUDA_TORCH_IMG} --build-arg "BASE_IMAGE=${L4T_SKIMAGE_CUDA_IMG}" -f Dockerfile.pytorch .
+JP_44_SKIMAGE_CUDA_IMG=jetson44:skimage-cuda
+JP_44_SKIMAGE_CUDA_TORCH_IMG=jetson44:skimage-cuda-torch
+JP_44_SKIMAGE_CUDA_TORCH_GST_OPENCV_IMG=jetson44:skimage-cuda-torch-gst-opencv
+JP_44_SKIMAGE_CUDA_TF_IMG=jetson44:skimage-cuda-tf
 
-docker build -t ${L4T_SKIMAGE_CUDA_GST_OPENCV_IMG} --build-arg "BASE_IMAGE=${L4T_SKIMAGE_CUDA_IMG}" -f Dockerfile.gstreamer-opencv .
+docker build -t $JP_44_SKIMAGE_CUDA_IMG --build-arg "BASE_IMAGE=${JETSON_SKIMAGE_IMG}" -f ./4.4/Dockerfile.cuda .
+docker build -t $JP_44_SKIMAGE_CUDA_TORCH_IMG --build-arg "BASE_IMAGE=${JP_44_SKIMAGE_CUDA_IMG}" -f ./4.4/Dockerfile.pytorch .
+docker build -t ${JP_44_SKIMAGE_CUDA_TORCH_GST_OPENCV_IMG} --build-arg "BASE_IMAGE=${JP_44_SKIMAGE_CUDA_TORCH_IMG}" -f Dockerfile.gstreamer-opencv .
+docker build -t ${JP_44_SKIMAGE_CUDA_TF_IMG} --build-arg "BASE_IMAGE=${JP_44_SKIMAGE_CUDA_IMG}" --build-arg "JP_VERSION=44" -f Dockerfile.tf .
 
-docker build -t ${L4T_SKIMAGE_CUDA_TF_IMG} --build-arg "BASE_IMAGE=${L4T_SKIMAGE_CUDA_IMG}" --build-arg "JP_VERSION=${JP_VERSION}" -f Dockerfile.tf .
+JP_43_SKIMAGE_CUDA_IMG=jetson43:skimage-cuda
+JP_43_SKIMAGE_CUDA_TORCH_IMG=jetson43:skimage-cuda-torch
+JP_43_SKIMAGE_CUDA_TORCH_GST_OPENCV_IMG=jetson43:skimage-cuda-torch-gst-opencv
+JP_43_SKIMAGE_CUDA_TORCH_GST_OPENCV_SKLEARN_IMG=jetson43:skimage-cuda-torch-gst-opencv-sklearn
+JP_43_SKIMAGE_CUDA_TF_IMG=jetson43:skimage-cuda-tf
+
+
+docker build -t $JP_43_SKIMAGE_CUDA_IMG --build-arg "BASE_IMAGE=${JETSON_SKIMAGE_IMG}" -f ./4.3/Dockerfile.cuda .
+docker build -t $JP_43_SKIMAGE_CUDA_TORCH_IMG --build-arg "BASE_IMAGE=${JP_43_SKIMAGE_CUDA_IMG}" -f ./4.3/Dockerfile.pytorch .
+docker build -t ${JP_43_SKIMAGE_CUDA_TORCH_GST_OPENCV_IMG} --build-arg "BASE_IMAGE=${JP_43_SKIMAGE_CUDA_TORCH_IMG}" -f Dockerfile.gstreamer-opencv .
+docker build -t ${JP_43_SKIMAGE_CUDA_TORCH_GST_OPENCV_SKLEARN_IMG} --build-arg "BASE_IMAGE=${JP_43_SKIMAGE_CUDA_TORCH_GST_OPENCV_IMG}" -f Dockerfile.sklearn .
+docker build -t ${JP_43_SKIMAGE_CUDA_TF_IMG} --build-arg "BASE_IMAGE=${JP_43_SKIMAGE_CUDA_IMG}" --build-arg "JP_VERSION=43" -f Dockerfile.tf .
